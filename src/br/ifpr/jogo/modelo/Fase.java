17e6e9ffe9cb9103501b13f2f2a5174c15ab4015
Estudante <estudante@ifpr.com>
2023-05-23 22:45:03 -0300

    Criação do fundo de imagem

---
+++ src/br/ifpr/jogo/modelo/Fase.java
@@ -0,0 +1,11 @@
+package br.ifpr.jogo.modelo;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    private Image fundo;
+
+    public Fase(){
+        ImageIcon carregando = new ImageIcon();
+    }
+}
